759a43947481193da6637817f390b5702063e539
Wendy <alteredco@gmail.com>
2020-03-30 12:16:24 +0100

    update gitignore

--- GasMon/Program.cs
+++ GasMon/Program.cs
@@ -8,7 +8,6 @@ namespace GasMon
         private static string bucketName;
         private static string topicArn;
         private static string region;
-        
         private static string keyName;
         
         static void Main(string[] args)
