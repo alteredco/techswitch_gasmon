16065758be4775ca3677d2694dfee9f7d5a99ea7
Wendy <alteredco@gmail.com>
2020-03-30 11:44:22 +0100

    initial commit

---
+++ GasMon/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Amazon;
+
+namespace GasMon
+{
+    class Program
+    {
+        private static string bucketName;
+        private static string topicArn;
+        private static string region;
+        
+        private static string keyName;
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
