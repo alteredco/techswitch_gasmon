585b1debef6325094039e425b1e36b9eeb2154c0
Wendy <alteredco@gmail.com>
2020-03-31 06:03:12 +0100

    add s3Request and location model

---
+++ GasMon/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace GasMon
+{
+    public class Location
+    {
+        public string Id { get; set; }
+        public double x { get; set; }
+        public double y { get; set; }
+    }
+}
--- GasMon/Program.cs
+++ GasMon/Program.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 namespace GasMon
 {
     class Program
     {
-        private static string bucketName;
-        private static string topicArn;
-        private static string region;
-        private static string keyName;
-        
         static void Main(string[] args)
         {
+            var s3Request = new S3Request();
+            var locations = s3Request.FetchLocations();
             
+            Console.WriteLine(locations);
+            Console.ReadLine();
         }
     }
 }
